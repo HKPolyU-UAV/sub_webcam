c73164792d8c3c8a3dc87115a3c9a3c70b86ccb4
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2022-10-29 14:49:19 +0800

    second commit

--- offb/src/camera.cpp
+++ offb/src/camera.cpp
@@ -9,7 +9,11 @@
 #include "geometry_msgs/PointStamped.h"
 
 
-#include "include/run_yolo.h"
+// #include "include/run_yolo.h"
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <string>
 #include "offb/obj.h"
 
@@ -20,8 +24,11 @@ static int counter = 0;
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "yolotiny");
+    ros::init(argc, argv, "sub_webcam");
     ros::NodeHandle nh;
+
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub = it.advertise("/ardusub/image", 1);
 
     cv::VideoCapture capture(0);
  
@@ -32,24 +39,27 @@ int main(int argc, char** argv)
 	}
  
 	cv::Mat frame;
-	cv::namedWindow("video test");
+	// cv::namedWindow("video test");
  
 	int frame_num = capture.get(cv::CAP_PROP_FRAME_COUNT);
 	std::cout << "total frame number is: " << frame_num << std::endl;
 
-    while (true)
+    while (ros::ok())
     {
         capture >> frame;
-        cv::imshow("hi", frame);
-        cv::waitKey(20);
+        // cv::imshow("hi", frame);
+        // cv::waitKey(20);
+        cout<<frame.size()<<endl;
+
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+        pub.publish(msg);
+        ros::spinOnce();
     }
     
  
-	cv::destroyWindow("video test");
+	// cv::destroyWindow("video test");
 	capture.release();
 
-
-    ros::spin();
     return 0;
 }
 
